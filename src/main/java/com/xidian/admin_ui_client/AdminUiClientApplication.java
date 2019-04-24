0ca29a52b389eb5d47c76e2e8aab6ac7cd79b1c2
wangshengsheng <1364839598@qq.com>
2019-04-24 17:44:13 +0800

    这个是监控admin_ui_client的客户端，需要和服务端一块使用，使用时先开启服务端再开启客户端，服务端对应admin_ui这个项目，这两个结合同时使用

---
+++ src/main/java/com/xidian/admin_ui_client/AdminUiClientApplication.java
@@ -0,0 +1,13 @@
+package com.xidian.admin_ui_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminUiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminUiClientApplication.class, args);
+    }
+
+}
